fc9e58c69cb41782bd306d45ff0e9afe209afa5f
GumnovRuslan <ruslang.crocode@gmail.com>
2024-05-20 13:47:15 +0300

    added google analytics

--- src/lib/components/FormControls.svelte
+++ src/lib/components/FormControls.svelte
@@ -1,37 +1,6 @@
 <script>
-	export let buttonsControls
-	export let next = true
-
-    function checkForm(e) {
-        let form = e.target.closest('form')
-		let inputs = form.querySelectorAll('input[required]');
-		inputs.forEach((el) => {
-			if (el.type == 'checkbox') {
-				if (!el.checked) {
-					el.classList.add('invalid-checkbox');
-				} else {
-					el.classList.remove('invalid-checkbox');
-				}
-			} else {
-				if (el.value == '') {
-					el.classList.add('invalid-input');
-				} else {
-					el.classList.remove('invalid-input');
-				}
-			}
-		})
-	}
-
-    function prevStep(e) {
-        let formTarget = e.target.closest('form')
-        let forms = document.querySelectorAll('form.form-stage')
-		let progress = document.querySelectorAll('.progress__bar')
-        let index = 0
-        forms.forEach((form, i)=> {if(form == formTarget) index = i})
-        formTarget.style.display = 'none'
-        forms[index - 1].style.display = 'flex'
-        progress[index].classList.remove('progress__bar--completed')
-    }
+	export let buttonsControls;
+	export let next = true;
 </script>
 
 <div class='controls'>
